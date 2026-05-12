93fde690c8996073a7539b728c2f8b1d4d2c5605
Vachan Bhogi <vachanbhogi@outlook.com>
2026-05-11 20:34:54 -0700

    chore: update base path for renamed repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://vachanbhogi.github.io',
-  base: 'vachanbhogi-portfolio',
+  base: 'portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
